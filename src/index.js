f95de283acbbe05befd2524e46c237b11f905716
VadymMakovii <makovii.vadym@gmail.com>
2023-03-21 13:20:18 +0200

    Init app

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { App } from 'components/App/App';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/goit-react-hw-08-phonebook">
+      <BrowserRouter basename="/phonebook-app">
         <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
